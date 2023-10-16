1feee3d01e1264fc41adb2880b8b5af55feb15d3
Cyle Witruk <cyle.witruk@outlook.com>
2023-10-17 01:41:03 +0200

    cleanup

--- src/parser.rs
+++ src/parser.rs
@@ -1,170 +1,9 @@
-use chumsky::{input::ValueInput, prelude::*, extra::Full};
+use chumsky::{input::ValueInput, prelude::*};
 
 use crate::{
     ast::SExpr, 
-    lexer::Token, signatures::{MapSignature, FunctionSignature}, ast::{Literal, Define, Type, MapDef, FuncArg, FuncSignature, Op, DefaultToDef}, types::{FunctionKind, ClarityInteger}
+    lexer::Token, ast::{Literal, Define, Type, MapDef, FuncArg, FuncSignature, Op, DefaultToDef}, types::FunctionKind
 };
-
-pub struct ParsingState {
-    maps: Vec<MapSignature>,
-    functions: Vec<FunctionSignature>
-}
-
-impl Default for ParsingState {
-    fn default() -> Self {
-        Self { 
-            maps: Default::default(),
-            functions: Default::default()
-        }
-    }
-}
-
-pub fn parser<'a, I>() -> impl Parser<
-    'a, 
-    I, 
-    SExpr, 
-    Full::<Rich<'a, Token>, 
-    ParsingState, ()>
->
-where
-    I: ValueInput<'a, Token = Token, Span = SimpleSpan>,
-{
-
-    recursive(|sexpr| {
-
-        let ident = select! { 
-            Token::Identifier(ident) => ident 
-        }.labelled("identifier");
-
-        let ty = select! {
-            Token::Int => Type::Int,
-            Token::UInt => Type::UInt,
-            Token::Principal => Type::Principal
-        }.labelled("type");
-
-        let fn_visibility = select! {
-            Token::OpDefinePublic => FunctionKind::Public,
-            Token::OpDefinePrivate => FunctionKind::Private,
-            Token::OpDefineReadOnly => FunctionKind::ReadOnly
-        }.labelled("function definition");
-
-        
-
-        let fn_signature = ident
-            .then(ident.then(ty))
-            .delimited_by(just(Token::ParenOpen), just(Token::ParenClose))
-            .repeated();
-
-        let items = sexpr
-            .clone()
-            .separated_by(just(Token::Whitespace))
-            .allow_leading()
-            .allow_trailing()
-            .collect::<Vec<_>>();
-
-        let define_map = just(Token::OpDefineMap) 
-            .ignore_then(ident)
-            .then(ty.labelled("key-type"))
-            .then(ty.labelled("value-type"))
-            .delimited_by(just(Token::ParenOpen), just(Token::ParenClose))
-            .map(|((name, key_ty), val_ty)| {
-                eprintln!("define-map {} {:?} {:?}", name, key_ty, val_ty);
-                SExpr::Define(Define::Map(MapDef { name, key_ty, val_ty }))
-            });
-
-        let define_fn = fn_visibility
-            .then(fn_signature)
-            .delimited_by(just(Token::ParenOpen), just(Token::ParenClose))
-            .map(|x| {
-                todo!()
-            });
-            
-            
-
-        // Define functions
-        /*let top_level_fns = select! {
-            Token::OpDefineMap => SExpr::Define(Define::Map),
-            Token::OpDefineReadOnly => SExpr::Define(Define::ReadOnlyFunction),
-            Token::OpDefinePublic => SExpr::Define(Define::PublicFunction),
-            Token::OpDefinePrivate => SExpr::Define(Define::PrivateFunction),
-            Token::OpDefineDataVar => SExpr::Define(Define::DataVar),
-        };
-
-        let keywords = select! {
-            Token::BlockHeight => SExpr::Keyword(Keyword::BlockHeight),
-            Token::BurnBlockHeight => SExpr::Keyword(Keyword::BurnBlockHeight),
-            Token::ChainId => SExpr::Keyword(Keyword::ChainId),
-            Token::ContractCaller => SExpr::Keyword(Keyword::ContractCaller),
-            Token::False => SExpr::Keyword(Keyword::False),
-            Token::IsInMainnet => SExpr::Keyword(Keyword::IsInMainnet),
-            Token::IsInRegTest => SExpr::Keyword(Keyword::IsInRegTest),
-            Token::None => SExpr::Keyword(Keyword::None),
-            Token::StxLiquidSupply => SExpr::Keyword(Keyword::StxLiquidSupply),
-            Token::True => SExpr::Keyword(Keyword::True),
-            Token::TxSender => SExpr::Keyword(Keyword::TxSender),
-            Token::TxSponsorOpt => SExpr::Keyword(Keyword::TxSponsor),
-        };
-
-        let identifier = select! {
-            Token::Identifier(name) => SExpr::Identifier(name)
-        };
-        
-        let types = select! {
-            Token::Int => SExpr::TypeDef(Type::Int),
-            Token::UInt => SExpr::TypeDef(Type::UInt),
-            Token::Principal => SExpr::TypeDef(Type::Principal),
-            Token::RefinedInteger(x) => SExpr::RefinedInteger(x),
-            Token::AsciiString(len) => SExpr::TypeDef(Type::StringAscii(len)),
-            Token::Utf8String(len) => SExpr::TypeDef(Type::StringUtf8(len))
-        };
-
-        let ops = select! {
-            Token::OpAdd => SExpr::Op(Op::Add),
-            Token::OpSub => SExpr::Op(Op::Sub),
-            Token::OpMul => SExpr::Op(Op::Mul),
-            Token::OpDiv => SExpr::Op(Op::Div),
-            Token::OpDefaultTo => SExpr::Op(Op::DefaultTo),
-            Token::OpMapGetOpt => SExpr::Op(Op::MapGet),
-            Token::OpOk => SExpr::Op(Op::Ok),
-            Token::OpMapSet => SExpr::Op(Op::MapSet),
-        };
-
-        let literals = select! {
-            Token::Int => SExpr::Literal(Literal::Int),
-            Token::UInt => SExpr::Literal(Literal::UInt),
-            Token::LiteralInteger(int) => SExpr::Literal(Literal::Integer(int))
-        };
-        
-
-        let list = sexpr
-            .repeated()
-            .collect()
-            .map(SExpr::Closure)
-            .delimited_by(just(Token::ParenOpen), just(Token::ParenClose));
-
-        list.clone().map(|l| {
-            eprintln!("expr: {l:?}");
-        });*/
-
-        define_map
-            .or(define_fn)
-
-        /*top_level_fns
-            .or(keywords)
-            .or(ops)
-            .or(types)
-            .or(identifier)
-            .or(literals)
-            .or(list)*/
-    }).then_ignore(end())
-}
-
-// The type of the input that our parser operates on. The input is the `&[(Token, Span)]` token buffer generated by the
-// lexer, wrapped in a `SpannedInput` which 'splits' it apart into its constituent parts, tokens and spans, for chumsky
-// to understand.
-//type ParserInput<'tokens> =
- //   chumsky::input::SpannedInput<Token, Span, &'tokens [(Token, Span)]>;
-
 
 pub type Span = SimpleSpan<usize>;
 //pub type Spanned<T> = (T, Span);
@@ -180,29 +19,51 @@ where
     }
 }
 
+/// Parser for expressions
 fn expr_parser<'a, I>() -> 
-impl Parser<'a, I, SExpr, extra::Err<Rich<'a, Token>>>
+impl Parser<'a, I, SExpr, extra::Err<Rich<'a, Token>>> + Clone
 where
     I: ValueInput<'a, Token = Token, Span = SimpleSpan>
 {
     recursive(|expr| {
+
         let default_to = just(Token::OpDefaultTo)
             .ignore_then(literal_parser())
             .then(expr)
             .delimited_by(just(Token::ParenOpen), just(Token::ParenClose))
             .map(|(default, tail)| {
-                eprintln!("default: {default:?}, tail: {tail:?}");
+                eprintln!("default-to: {{ default: {default:?}, tail: {tail:?} }}");
                 SExpr::Op(Op::DefaultTo(DefaultToDef {
                     default, tail: Box::new(tail)
                 }))
             });
 
+        let map_get = just(Token::OpMapGetOpt)
+            .ignore_then(ident_parser())
+            .then(ident_parser())
+            .delimited_by(just(Token::ParenOpen), just(Token::ParenClose))
+            .map(|(map, key)| {
+                eprintln!("map-get?: {{ map: {map:?}, key: {key:?} }}");
+                SExpr::Op(Op::MapGet)
+            });
+
         default_to
+            .or(map_get)
     })
 }
 
+/// Parser for parsing identifiers.
+pub fn ident_parser<'tokens, 'src: 'tokens, I>() -> 
+impl Parser<'tokens, I, String, extra::Err<Rich<'tokens, Token, Span>>> + Clone
+where 
+    I: ValueInput<'tokens, Token = Token, Span = SimpleSpan>
+{
+    select! { Token::Identifier(ident) => ident }
+        .labelled("identifier")
+}
+
 pub fn top_level_parser<'tokens, 'src: 'tokens, I>() -> 
-impl Parser<'tokens, I, Vec<SExpr>, extra::Err<Rich<'tokens, Token, Span>>> 
+impl Parser<'tokens, I, Vec<SExpr>, extra::Err<Rich<'tokens, Token, Span>>> + Clone
 where 
     I: ValueInput<'tokens, Token = Token, Span = SimpleSpan>
 {
@@ -242,9 +103,7 @@ where
 
     let define_fn = func_visibility
         .then(func_signature)
-        .then(
-            expr_parser()
-        )
+        .then(expr_parser())
         .delimited_by(just(Token::ParenOpen), just(Token::ParenClose))
         .map(|(kind, sig)| {
             eprintln!("define_fn: {kind:?}->{sig:?}");
